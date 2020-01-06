3b3508a3262636af5da4bda334da78923434f607
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-01-06 21:26:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,5 +6,18 @@ class Person
   end
 end
 
-def Person
+rj_attributes = {name: "RJ", birthday: "August 2", hair_color: "Black"}
+
+rj = Person.new(rj_attributes)
+rj.name 
+rj.birthday
+rj.hair_color
+
+shasha_attributes = {name: "Shasha", eye_color: "Brown", height: "5'4\"", handed: "Right"}
+
+shasha = Person.new(shasha_attributes)
+shasha.name
+shasha.eye_color
+shasha.height
+shasha.handed
 
